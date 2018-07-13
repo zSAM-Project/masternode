dcd973cf169c53516e1c3c65481391299da739c5
zSAM-Project <40679306+zSAM-Project@users.noreply.github.com>
2018-07-13 13:39:56 +0700

    Add files via upload

---
+++ stamp_mn_install.sh
@@ -0,0 +1,112 @@
+#/bin/bash
+
+################################################################################
+# Author:   Supawat A
+# Date:     July, 16th 2018
+# 
+# Program:
+#
+#   Install STAMP Coin masternode on clean VPS with Ubuntu 16.04 
+#
+################################################################################
+
+echo && echo 
+echo "****************************************************************************"
+echo "*                                                                          *"
+echo "*  This script will install and configure your STAMP Coin masternode.      *"
+echo "*                                                                          *"
+echo "****************************************************************************"
+echo && echo
+
+set -o errexit
+
+cd ~
+sudo locale-gen en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+STAMP_LINUX_URL=https://github.com/zSAM-Project/stamp/releases/download/1.1.0.2/stamp-1.1.0-x86_64-linux-gnu.tar.gz
+STAMP_USER_PASS=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 ; echo ""`
+STAMP_RPC_PASS=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 24 ; echo ""`
+MN_NAME_PREFIX=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 6 ; echo ""`
+MN_EXTERNAL_IP=`curl -s -4 ifconfig.co`
+
+sudo useradd -U -m stampcoin -s /bin/bash
+echo "stampcoin:${STAMP_USER_PASS}" | sudo chpasswd
+sudo wget $STAMP_LINUX_URL --directory-prefix /home/stampcoin/
+sudo tar -xzvf /home/stampcoin/stamp-*-x86_64-linux-gnu.tar.gz -C /home/stampcoin/
+sudo rm /home/stampcoin/stamp-*-x86_64-linux-gnu.tar.gz
+sudo chown -R stampcoin:stampcoin /home/stampcoin/stamp*
+sudo chmod 755 /home/stampcoin/stamp*
+echo "Copy STAMP files!"
+sudo cp /home/stampcoin/stamp*/bin/stampd /usr/bin
+sudo cp /home/stampcoin/stamp*/bin/stamp-cli /usr/bin
+sudo rm -rf /home/stampcoin/stamp*
+
+CONF_DIR=/home/stampcoin/.stamp/
+CONF_FILE=stamp.conf
+
+mkdir -p $CONF_DIR
+echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> $CONF_DIR/$CONF_FILE
+echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> $CONF_DIR/$CONF_FILE
+echo "rpcallowip=127.0.0.1" >> $CONF_DIR/$CONF_FILE
+echo "rpcport=33453" >> $CONF_DIR/$CONF_FILE
+echo "listen=1" >> $CONF_DIR/$CONF_FILE
+echo "server=1" >> $CONF_DIR/$CONF_FILE
+echo "daemon=1" >> $CONF_DIR/$CONF_FILE
+echo "maxconnections=256" >> $CONF_DIR/$CONF_FILE
+echo "port=33452" >> $CONF_DIR/$CONF_FILE
+echo "masternode=1" >> $CONF_DIR/$CONF_FILE
+echo "masternodeaddress=$MN_EXTERNAL_IP:33452" >> $CONF_DIR/$CONF_FILE
+
+sudo chown -R stampcoin:stampcoin /home/stampcoin/.stamp/
+sudo chown 500 /home/stampcoin/.stamp/stamp.conf
+
+sudo tee /etc/systemd/system/stampcoin.service <<EOF
+[Unit]
+Description=STAMPCoin, distributed currency daemon
+After=network.target
+
+[Service]
+User=stampcoin
+Group=stampcoin
+WorkingDirectory=/home/stampcoin/
+ExecStart=/usr/bin/stampd
+
+Restart=always
+PrivateTmp=true
+TimeoutStopSec=60s
+TimeoutStartSec=2s
+StartLimitInterval=120s
+StartLimitBurst=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl enable stampcoin
+sudo systemctl start stampcoin
+echo "Booting STAMP node and creating keypool"
+sleep 120
+
+MNGENKEY=`sudo -H -u stampcoin /usr/bin/stamp-cli masternode genkey`
+echo -e "masternodeprivkey=${MNGENKEY}" | sudo tee -a /home/stampcoin/.stamp/stamp.conf
+sudo systemctl restart stampcoin
+
+echo " "
+echo " "
+echo "==============================="
+echo "Masternode installed!"
+echo "==============================="
+echo "Copy and keep that information in secret:"
+echo "Masternode key: ${MNGENKEY}"
+echo "SSH password for user \"stampcoin\": ${STAMP_USER_PASS}"
+echo "Prepared masternode.conf string:"
+echo "MN_${MN_NAME_PREFIX} ${MN_EXTERNAL_IP}:3234 ${MNGENKEY} INPUTTX INPUTINDEX"
+
+exit 0
+
+
+
---
+++ vps_setup.sh
@@ -0,0 +1,66 @@
+#/bin/bash
+
+################################################################################
+# Author:   Supawat A
+# Date:     July, 16th 2018
+# 
+# Program:
+#
+#   Prepare Masternode on clean VPS with Ubuntu 16.04 
+#
+################################################################################
+
+echo && echo 
+echo "****************************************************************************"
+echo "*                                                                          *"
+echo "*  This script will install the package need for your STAMP Coin.          *"
+echo "*                                                                          *"
+echo "****************************************************************************"
+echo && echo
+
+# Install necessary stuff
+
+cd ~
+sudo locale-gen en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get install -y nano htop git
+sudo apt-get install -y software-properties-common
+sudo apt-get install -y build-essential libtool autotools-dev pkg-config libssl-dev
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y libevent-dev
+sudo apt-get install -y libzmq3-dev
+sudo apt-get install -y libminiupnpc-dev
+sudo apt-get install -y autoconf
+sudo apt-get install -y automake unzip
+sudo add-apt-repository  -y  ppa:bitcoin/bitcoin
+sudo apt-get update
+sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
+sudo apt-get install -y  curl wget git python3 python3-pip python-virtualenv
+
+cd /var
+sudo touch swap.img
+sudo chmod 600 swap.img
+sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+sudo mkswap /var/swap.img
+sudo swapon /var/swap.img
+sudo free
+sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+cd
+
+sudo apt-get install -y ufw
+sudo ufw allow ssh/tcp
+sudo ufw limit ssh/tcp
+sudo ufw allow 33452/tcp
+sudo ufw allow 33453/tcp
+sudo ufw logging on
+sudo ufw enable
+sudo ufw status
+
+echo "Your VPS for Masternode is now ready!"
