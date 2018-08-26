cacad9b6a05e27e8ee397b05f84933139f5aee47
hexter2018 <38841538+hexter2018@users.noreply.github.com>
2018-08-26 20:50:26 +0700

    Update mn_stampcoin_update.sh

--- mn_stampcoin_update.sh
+++ mn_stampcoin_update.sh
@@ -18,12 +18,12 @@ echo "**************************************************************************
 echo && echo
 
 cd ~
-sudo wget https://github.com/zSAM-Project/stamp/releases/download/masternode/stamp.tar.gz
-sudo tar -xzvf stamp.tar.gz
+sudo wget https://github.com/zSAM-Project/stamp/releases/download/v2.4.1.1/stamp-2.4.1.tar.gz
+sudo tar -xzvf stamp-2.4.1.tar.gz
 sudo systemctl stop stampcoin
 sleep 10
 sudo cp ~/stamp/stamp* /usr/local/bin/
-sudo rm ~/stamp.tar.gz 
+sudo rm ~/stamp-2.4.1.tar.gz
 sudo rm -rf /root/stamp
 sudo systemctl start stampcoin
 sleep 10
