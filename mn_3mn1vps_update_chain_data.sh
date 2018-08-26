dab95776d56ad99609c4835c4d232ecb33a243e7
zSAM-Project <40679306+zSAM-Project@users.noreply.github.com>
2018-08-26 21:16:28 +0700

    Update mn_3mn1vps_update_chain_data.sh

--- mn_3mn1vps_update_chain_data.sh
+++ mn_3mn1vps_update_chain_data.sh
@@ -18,17 +18,17 @@ echo "**************************************************************************
 echo && echo
 
 cd ~
-sudo wget https://github.com/zSAM-Project/stamp/releases/download/v2.4.1.1/stamp-2.4.1.tar.gz
-sudo tar -xzvf stamp-2.4.1.tar.gz
+sudo wget https://github.com/zSAM-Project/stamp/releases/download/masternode/stamp.tar.gz
+sudo tar -xzvf stamp.tar.gz
 sudo systemctl stop stampmn1
 sudo systemctl stop stampmn2
 sudo systemctl stop stampmn3
-sleep 10
+sleep 15
 
 sudo cp ./stamp/stamp* /home/stampmn1/
 sudo cp ./stamp/stamp* /home/stampmn2/
 sudo cp ./stamp/stamp* /home/stampmn3/
-sudo rm ./stamp-2.4.1.tar.gz
+sudo rm ./stamp.tar.gz
 sudo rm -rf ./stamp
 
 
@@ -71,6 +71,7 @@ sudo systemctl start stampmn1
 sudo systemctl start stampmn2
 sudo systemctl start stampmn3
 sleep 30
+
 INFO1=`sudo -H -u stampmn1 /home/stampmn1/stamp-cli getinfo`
 INFO2=`sudo -H -u stampmn2 /home/stampmn2/stamp-cli getinfo`
 INFO3=`sudo -H -u stampmn3 /home/stampmn3/stamp-cli getinfo`
