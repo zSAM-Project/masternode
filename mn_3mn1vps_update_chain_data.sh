3b8ce552108b965fb87dea23dcca703e40055970
hexter2018 <38841538+hexter2018@users.noreply.github.com>
2018-08-26 20:47:29 +0700

    Update mn_3mn1vps_update_chain_data.sh

--- mn_3mn1vps_update_chain_data.sh
+++ mn_3mn1vps_update_chain_data.sh
@@ -18,8 +18,8 @@ echo "**************************************************************************
 echo && echo
 
 cd ~
-sudo wget https://github.com/zSAM-Project/stamp/releases/download/masternode/stamp.tar.gz
-sudo tar -xzvf stamp.tar.gz
+sudo wget https://github.com/zSAM-Project/stamp/releases/download/v2.4.1.1/stamp-2.4.1.tar.gz
+sudo tar -xzvf stamp-2.4.1.tar.gz
 sudo systemctl stop stampmn1
 sudo systemctl stop stampmn2
 sudo systemctl stop stampmn3
@@ -28,58 +28,44 @@ sleep 10
 sudo cp ./stamp/stamp* /home/stampmn1/
 sudo cp ./stamp/stamp* /home/stampmn2/
 sudo cp ./stamp/stamp* /home/stampmn3/
-sudo rm ./stamp.tar.gz 
+sudo rm ./stamp-2.4.1.tar.gz
 sudo rm -rf ./stamp
 
 
-sudo wget https://github.com/zSAM-Project/stamp/releases/download/masternode/chain.tar.gz
-sudo tar -xzvf chain.tar.gz
-sudo rm ./chain.tar.gz
-
-#sudo mv /home/stampmn1/.stamp/blocks /home/stampmn1/.stamp/blocks_bak
-#sudo mv /home/stampmn1/.stamp/chainstate /home/stampmn1/.stamp/chainstate_bak
-
-#sudo mv /home/stampmn2/.stamp/blocks /home/stampmn2/.stamp/blocks_bak
-#sudo mv /home/stampmn2/.stamp/chainstate /home/stampmn2/.stamp/chainstate_bak
-
-#sudo mv /home/stampmn3/.stamp/blocks /home/stampmn3/.stamp/blocks_bak
-#sudo mv /home/stampmn3/.stamp/chainstate /home/stampmn3/.stamp/chainstate_bak
+sudo wget https://github.com/zSAM-Project/stamp/releases/download/v2.4.1.1/stamp-chain-40900-bootstrap.dat.zip
+sudo unzip stamp-chain-40900-bootstrap.dat.zip
+sudo rm ./stamp-chain-40900-bootstrap.dat.zip
 
 sudo rm -rf /home/stampmn1/.stamp/blocks
-sudo rm -rf /home/stampmn2/.stamp/blocks
-sudo rm -rf /home/stampmn3/.stamp/blocks
-
-sudo cp -r ./blocks /home/stampmn1/.stamp/
-sudo cp -r ./blocks /home/stampmn2/.stamp/
-sudo cp -r ./blocks /home/stampmn3/.stamp/
-
 sudo rm -rf /home/stampmn1/.stamp/chainstate
-sudo rm -rf /home/stampmn2/.stamp/chainstate
-sudo rm -rf /home/stampmn3/.stamp/chainstate
-
-sudo cp -r ./chainstate /home/stampmn1/.stamp/
-sudo cp -r ./chainstate /home/stampmn2/.stamp/
-sudo cp -r ./chainstate /home/stampmn3/.stamp/
-
-sudo rm -rf ./blocks
-sudo rm -rf ./chainstate
-
-sudo rm /home/stampmn1/.stamp/peers.dat 
-sudo rm /home/stampmn2/.stamp/peers.dat 
-sudo rm /home/stampmn3/.stamp/peers.dat 
-
+sudo rm -rf /home/stampmn1/.stamp/zerocoin
+sudo rm -rf /home/stampmn1/.stamp/sporks
+sudo rm /home/stampmn1/.stamp/peers.dat
 sudo rm /home/stampmn1/.stamp/banlist.dat 
+
+sudo rm -rf /home/stampmn2/.stamp/blocks
+sudo rm -rf /home/stampmn2/.stamp/chainstate
+sudo rm -rf /home/stampmn2/.stamp/zerocoin
+sudo rm -rf /home/stampmn2/.stamp/sporks
+sudo rm /home/stampmn2/.stamp/peers.dat
 sudo rm /home/stampmn2/.stamp/banlist.dat 
+
+sudo rm -rf /home/stampmn3/.stamp/blocks
+sudo rm -rf /home/stampmn3/.stamp/chainstate
+sudo rm -rf /home/stampmn3/.stamp/zerocoin
+sudo rm -rf /home/stampmn3/.stamp/sporks
+sudo rm /home/stampmn3/.stamp/peers.dat
 sudo rm /home/stampmn3/.stamp/banlist.dat 
 
-sudo chown -R stampmn1:stampmn1 /home/stampmn1/.stamp/blocks
-sudo chown -R stampmn1:stampmn1 /home/stampmn1/.stamp/chainstate
+sudo cp -r /root/bootstrap.dat /home/stampmn1/.stamp/
+sudo cp -r /root/bootstrap.dat /home/stampmn2/.stamp/
+sudo cp -r /root/bootstrap.dat /home/stampmn3/.stamp/
 
-sudo chown -R stampmn2:stampmn2 /home/stampmn2/.stamp/blocks
-sudo chown -R stampmn2:stampmn2 /home/stampmn2/.stamp/chainstate
+sudo chown -R stampmn1:stampmn1 /home/stampmn1/.stamp/bootstrap.dat
+sudo chown -R stampmn2:stampmn2 /home/stampmn2/.stamp/bootstrap.dat
+sudo chown -R stampmn3:stampmn3 /home/stampmn3/.stamp/bootstrap.dat
 
-sudo chown -R stampmn3:stampmn3 /home/stampmn3/.stamp/blocks
-sudo chown -R stampmn3:stampmn3 /home/stampmn3/.stamp/chainstate
+rm bootstrap.dat
 
 sudo systemctl start stampmn1
 sudo systemctl start stampmn2
