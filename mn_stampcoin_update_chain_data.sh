4140de675aa514c4383e9b860a0e6836a6a8a74f
zSAM-Project <40679306+zSAM-Project@users.noreply.github.com>
2018-08-26 21:13:50 +0700

    Update mn_stampcoin_update_chain_data.sh

--- mn_stampcoin_update_chain_data.sh
+++ mn_stampcoin_update_chain_data.sh
@@ -18,22 +18,22 @@ echo "**************************************************************************
 echo && echo
 
 cd ~
-sudo wget https://github.com/zSAM-Project/stamp/releases/download/v2.4.1.1/stamp-2.4.1.tar.gz
-sudo tar -xzvf stamp-2.4.1.tar.gz
+sudo wget https://github.com/zSAM-Project/stamp/releases/download/masternode/stamp.tar.gz
+sudo tar -xzvf stamp.tar.gz
 
 sudo systemctl stop stampcoin
 sleep 15
 
 sudo cp ./stamp/stamp* /usr/local/bin/
-sudo rm ./stamp-2.4.1.tar.gz
+sudo rm ./stamp.tar.gz
 sudo rm -rf ./stamp
 
 sudo wget https://github.com/zSAM-Project/stamp/releases/download/v2.4.1.1/stamp-chain-40900-bootstrap.dat.zip
 sudo unzip stamp-chain-40900-bootstrap.dat.zip
 
-sudo cp -r /root/stamp-chain-40900-bootstrap.dat /home/stampcoin/.stamp/
+sudo cp -r /root/bootstrap.dat /home/stampcoin/.stamp/
 
-sudo chown -R stampcoin:stampcoin /home/stampcoin/.stamp/stamp-chain-40900-bootstrap.dat
+sudo chown -R stampcoin:stampcoin /home/stampcoin/.stamp/bootstrap.dat
 
 sudo rm -rf /home/stampcoin/.stamp/blocks
 sudo rm -rf /home/stampcoin/.stamp/chainstate
