e65d2f41e8be3fa9bb8de5fae12774e269b075f2
hexter2018 <38841538+hexter2018@users.noreply.github.com>
2018-08-26 20:51:51 +0700

    Update mn_stampcoin_update_chain_data.sh

--- mn_stampcoin_update_chain_data.sh
+++ mn_stampcoin_update_chain_data.sh
@@ -18,37 +18,32 @@ echo "**************************************************************************
 echo && echo
 
 cd ~
-sudo wget https://github.com/zSAM-Project/stamp/releases/download/masternode/stamp.tar.gz
-sudo tar -xzvf stamp.tar.gz
+sudo wget https://github.com/zSAM-Project/stamp/releases/download/v2.4.1.1/stamp-2.4.1.tar.gz
+sudo tar -xzvf stamp-2.4.1.tar.gz
 
 sudo systemctl stop stampcoin
 sleep 15
 
 sudo cp ./stamp/stamp* /usr/local/bin/
-sudo rm ./stamp.tar.gz
+sudo rm ./stamp-2.4.1.tar.gz
 sudo rm -rf ./stamp
 
-sudo wget https://github.com/zSAM-Project/stamp/releases/download/masternode/chain.tar.gz
-sudo tar -xzvf chain.tar.gz
-sudo rm ./chain.tar.gz
+sudo wget https://github.com/zSAM-Project/stamp/releases/download/v2.4.1.1/stamp-chain-40900-bootstrap.dat.zip
+sudo unzip stamp-chain-40900-bootstrap.dat.zip
 
-#sudo mv /home/stampcoin/.stamp/blocks /home/stampcoin/.stamp/blocks_bak
-#sudo mv /home/stampcoin/.stamp/chainstate /home/stampcoin/.stamp/chainstate_bak
+sudo cp -r /root/stamp-chain-40900-bootstrap.dat /home/stampcoin/.stamp/
+
+sudo chown -R stampcoin:stampcoin /home/stampcoin/.stamp/stamp-chain-40900-bootstrap.dat
 
 sudo rm -rf /home/stampcoin/.stamp/blocks
 sudo rm -rf /home/stampcoin/.stamp/chainstate
-
-sudo cp -r ./blocks /home/stampcoin/.stamp/
-sudo cp -r ./chainstate /home/stampcoin/.stamp/
-
-sudo rm -rf ./blocks
-sudo rm -rf ./chainstate
-
-sudo rm /home/stampcoin/.stamp/peers.dat 
+sudo rm -rf /home/stampcoin/.stamp/zerocoin
+sudo rm -rf /home/stampcoin/.stamp/sporks
+sudo rm /home/stampcoin/.stamp/peers.dat
 sudo rm /home/stampcoin/.stamp/banlist.dat 
 
-sudo chown -R stampcoin:stampcoin /home/stampcoin/.stamp/blocks
-sudo chown -R stampcoin:stampcoin /home/stampcoin/.stamp/chainstate
+rm stamp-chain-40900-bootstrap.dat.zip
+rm bootstrap.dat
 
 sudo systemctl start stampcoin
 sleep 30
